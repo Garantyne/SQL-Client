e2bbee02afa51e873c048861d077994568060a0e
krela <krel.andrej@yandex.ru>
2024-02-27 15:37:17 +0700

    Какой то комит

--- SQL-Client/DB/DBQueryHandler.cs
+++ SQL-Client/DB/DBQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,26 @@ namespace SQL_Client.DB
     {
         public string Queri {  get; set; }
         public SqlConnection connection { get; set; }
+        public SqlDataAdapter adapter { get; set; }
 
         public DBQueryHandler(string text, SqlConnection con) {
             Queri = text;
             connection = con;
+            adapter = new SqlDataAdapter(Queri,connection);
         }
 
-        
+        public DataTable Query()
+        {
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        public string Query(string query)
+        {
+            SqlCommand command = new SqlCommand(query,connection);
+            int i = command.ExecuteNonQuery();
+            return $"Ваш запрос выполнил {i} команд";
+        }
     }
 }
